9fd6578c7f161acdedd0cf1f6a4daa37eb73cd05
PAVELNEDELKO1100 <pasan3090@gmail.com>
2023-08-06 18:21:51 +0600

    Init

---
+++ config.py
@@ -0,0 +1,12 @@
+#bot settings
+
+BOT_TOKEN = "6455601555:AAE2swq4mhkzgTpvXrAfolxDQJftR4UtI6c"
+BOT_OWNER = 6094982119 # for IsOwner filter
+
+# mysql settings
+
+host = "sql6.freemysqlhosting.net"
+port = 3306
+user = "sql6637053"
+password = "awGj7Apk2D"
+db_name = "sql6637053"
